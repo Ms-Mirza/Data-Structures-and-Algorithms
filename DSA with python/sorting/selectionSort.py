a682be6e20cea2ff19d55daffd45505f0c293ecf
Mirza Mohammad Salah Uddin <mirzamohammadsalahuddin@gmail.com>
2025-10-24 11:18:19 +0600

    selection sort for decending order

--- DSA with python/sorting/selectionSort.py
+++ DSA with python/sorting/selectionSort.py
@@ -1,11 +1,11 @@
 li = [1, 5, 6, 10, 11, 4, 3, 2, 8, 7]
 length = len(li)
 
-for i in range(0, length-1):
+for i in range(0, length):
     index = i
     
     for j in range (i+1, length):
-        if(li[index]>li[j]):
+        if(li[index]<li[j]): # decending order
             index = j
     
     li[index], li[i] = li[i], li[index]
